eee1826120f4ac3db5b2942a7875c027d4bdcab9
Lukaum <lucasandretto@gmail.com>
2017-11-25 04:14:35 -0200

    autoload hcodebr classes

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php 
 
+//require do composer
 require_once("vendor/autoload.php");
 
+//Configurado e chamando o slim
 $app = new \Slim\Slim();
 
+//Chamando a aplicação com modo debug, para mostrar erros
 $app->config('debug', true);
 
+//Rota com / a principal
 $app->get('/', function() {
-    
-	echo "OK";
+
+    //Mando executar a rota - Entro no vendor/Hcode e no namespace DB
+	$sql = new Hcode\DB\Sql();
+
+	$results = $sql->select("SELECT * FROM tb_users");
+	
+	echo json_encode($results);
 
 });
 
