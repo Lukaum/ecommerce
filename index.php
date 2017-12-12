f0a3f596de9a950c3ea384b592cd333d27d1f394
Lukaum <lucasandretto@gmail.com>
2017-12-12 05:29:28 -0200

    criamos a classe Page

--- index.php
+++ index.php
@@ -3,21 +3,22 @@
 //require do composer
 require_once("vendor/autoload.php");
 
-//Configurado e chamando o slim
-$app = new \Slim\Slim();
+//Namespaces - dentro do vendor eu tenho dezenas de classes
+use \Slim\Slim;
+use \Hcode\Page;
+
+//Configurado e chamando o slim - nova aplicação para as rotas (mando o nome para a url e ele me manda para algum lugar)
+$app = new Slim();
 
 //Chamando a aplicação com modo debug, para mostrar erros
 $app->config('debug', true);
 
-//Rota com / a principal
+//Rota com / a principal - Quando meu site tiver sem nenhum tipo de rota, carrega isso aqui
 $app->get('/', function() {
 
-    //Mando executar a rota - Entro no vendor/Hcode e no namespace DB
-	$sql = new Hcode\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
-	
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,65 @@
+<?php 
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Page {
+
+	private $tpl;
+	private $options = [];
+	private $defaults = [
+		"data"=>[]
+	];
+
+	public function __construct($opts = array()){
+
+		//array merge mescla 2 arrays - O último sempre sobrescreve os anteriores
+		$this->options = array_merge($this->defaults, $opts);
+
+		$config = array(
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"         => false // set to false to improve the speed
+	   	);
+
+		Tpl::configure( $config );		
+
+		$this->tpl = new Tpl;
+
+		$this->setData($this->options["data"]);
+
+		$this->tpl->draw("header");
+
+	}
+
+	//Passar os dados para meu template
+	private function setData($data = array())
+	{
+
+		foreach ($this->options["data"] as $key => $value) {
+			//Variáveis que vão pegar no template
+			$this->tpl->assign($key, $value);
+		}
+
+	}
+
+	public function setTpl($name, $data = array(), $returnHtml = false)
+	{
+
+		$this->setData($data);
+
+		return $this->tpl->draw($name, $returnHtml);
+
+	}
+
+	public function __destruct(){
+
+		$this->tpl->draw("footer");
+
+	}
+
+
+}
+
+ ?>
